5f3a88e3d6df947c87deb5b3022fc20f04314c13
Poweramo <assirouomar@gmail.com>
2023-09-06 12:43:13 +0000

    First sharing in github

---
+++ index.js
@@ -0,0 +1,27 @@
+const firstDice = document.getElementById("first-dice");
+const secondDice = document.getElementById("second-dice");
+
+const numbers = [1, 2, 3, 4, 5, 6];
+let index = Math.floor(Math.random() * 10);
+
+const rollingFirstDice = () => {
+	if (index < 6) {
+		firstDice.textContent = numbers[index];
+		index = Math.floor(Math.random() * 10);
+	} else {
+		index = Math.floor(Math.random() * 10);
+		rollingFirstDice();
+	}
+};
+const rollingSecondDice = () => {
+	if (index < 6) {
+		secondDice.textContent = numbers[index];
+		index = Math.floor(Math.random() * 10);
+	} else {
+		index = Math.floor(Math.random() * 10);
+		rollingSecondDice();
+	}
+};
+
+rollingFirstDice();
+rollingSecondDice();
